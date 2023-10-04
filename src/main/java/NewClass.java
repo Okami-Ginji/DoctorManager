9b3322f2bd5df54bf06dd721ad85e146e3efa36e
Administrator <Administrator@192.168.44.1>
2023-10-04 19:06:08 +0700

    Init project

---
+++ src/main/java/NewClass.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import controller.DoctorManagerProgram;
+/**
+ *
+ * @author Administrator
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        new DoctorManagerProgram().run();
+    }
+}
